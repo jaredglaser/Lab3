09ff0caabf19d334a43d4a53ba59c79fb709892f
jared <jared@LAPTOP-SR55OSKM>
2017-03-30 22:31:27 -0400

    Added Game

---
+++ BallysBLL/src/main/java/pkgPokerBLL/Game.java
@@ -0,0 +1,43 @@
+package pkgPokerBLL;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Game {
+	private UUID GameID;
+	private UUID TableID;
+	private ArrayList<Player> GamePlayers = new ArrayList<Player>();
+	
+	public Game(UUID gameID, UUID tableID, ArrayList<Player> gamePlayers) {
+		super();
+		GameID = gameID;
+		TableID = tableID;
+		GamePlayers = gamePlayers;
+	}
+
+	public UUID getGameID() {
+		return GameID;
+	}
+
+	public void setGameID(UUID gameID) {
+		GameID = gameID;
+	}
+
+	public UUID getTableID() {
+		return TableID;
+	}
+
+	public void setTableID(UUID tableID) {
+		TableID = tableID;
+	}
+
+	public ArrayList<Player> getGamePlayers() {
+		return GamePlayers;
+	}
+
+	public void setGamePlayers(ArrayList<Player> gamePlayers) {
+		GamePlayers = gamePlayers;
+	}
+	
+	
+}
